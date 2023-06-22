f3c6ae63b4ade4a8ac511278a26af6d28708c0a1
Boris Borovcanin <121176322+bb00S@users.noreply.github.com>
2023-06-22 13:25:02 +0200

    Update 2DiceProbability.java

--- 2DiceProbability.java
+++ 2DiceProbability.java
@@ -5,31 +5,31 @@ import java.util.Scanner;
 import java.util.Random; 
 public class proba{
 public static void main(String[] args) {
-Scanner unos=new Scanner(System.in);
-Random rn = new Random();
-int sims = 10000;
-int broj_bacanja = 0;
-for (int i=0;i<sims;i++){
-boolean b1 = false;
-boolean b2 = false;
-boolean b3 = false;
-int broj = 0;
-while (b1 == false || b2 == false || b3 == false) {
-int min=1;
-int max=6;
-int k1 = (1+rn.nextInt(6)); 
-int k2 = (1+rn.nextInt(6));
-int k = k1 + k2;
-broj +=1;
-b1 = b2;
-b2 = b3;
-if (k > 6) {
-b3 = true;}
-else {
-b3 = false;}
-}
-broj_bacanja += broj;
-}
-System.out.println(broj_bacanja / sims);
+  Scanner unos=new Scanner(System.in);
+  Random rn = new Random();
+  int sims = 10000;
+  int broj_bacanja = 0;
+  for (int i=0;i<sims;i++){
+    boolean b1 = false;
+    boolean b2 = false;
+    boolean b3 = false;
+    int broj = 0;
+    while (b1 == false || b2 == false || b3 == false) {
+      int min=1;
+      int max=6;
+      int k1 = (1+rn.nextInt(6)); 
+      int k2 = (1+rn.nextInt(6));
+      int k = k1 + k2;
+      broj +=1;
+      b1 = b2;
+      b2 = b3;
+      if (k > 6) {
+        b3 = true;}
+      else {
+        b3 = false;}
+    }
+    broj_bacanja += broj;
+  }
+  System.out.println(broj_bacanja / sims);
 }
 }
