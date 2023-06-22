c09213efc2ae984f7fc7882b67f4c50c3e509d19
Boris Borovcanin <121176322+bb00S@users.noreply.github.com>
2023-06-22 13:20:20 +0200

    Update LongestRange.java

--- LongestRange.java
+++ LongestRange.java
@@ -4,27 +4,26 @@ we have four ranges with digits greater than 4, namely: 7, 75, 786, 98. The long
 import java.util.Scanner;
 public class proba {
 public static void main(String[] args) {
-Scanner unos=new Scanner (System.in);
-long a = unos.nextLong();
-long max_raspon = 0;
-long t_raspon = 0;
-while (a > 0){
-long cifra = a % 10;
-if (cifra < 5) {
-if (t_raspon > max_raspon){
-max_raspon = t_raspon;
-
-}
-t_raspon = 0;
-}
-else{
-t_raspon += 1;
-}
-a /= 10;
-}
-if (t_raspon > max_raspon){
-max_raspon = t_raspon;
-}
-System.out.println(max_raspon);
+  Scanner unos=new Scanner (System.in);
+  long a = unos.nextLong();
+  long max_raspon = 0;
+  long t_raspon = 0;
+  while (a > 0){
+    long cifra = a % 10;
+    if (cifra < 5) {
+      if (t_raspon > max_raspon){
+        max_raspon = t_raspon;
+      }
+      t_raspon = 0;
+    }
+    else{
+      t_raspon += 1;
+    }
+    a /= 10;
+  }
+  if (t_raspon > max_raspon){
+    max_raspon = t_raspon;
+  }
+  System.out.println(max_raspon);
 }
 }
